f777f8169ba7e06267e9b70649027e8f1b23a797
Luiz Wendel <luiz.balbino@mercadolivre.com>
2024-03-01 14:16:09 -0300

    test: add Task run function test with fake timers

--- tajs/02-concepts/04-fake-timers/test/task.test.js
+++ tajs/02-concepts/04-fake-timers/test/task.test.js
@@ -17,7 +17,7 @@ describe('Task Suite', () => {
   });
 
   describe('run', () => {
-    it('should only run tasks that are due - without fake timers (slow)', async () => {
+    it.skip('should only run tasks that are due - without fake timers (slow)', async () => {
       const tasks = [
         {
           name: 'Task-Will-Run-In-5-Seconds',
@@ -40,5 +40,61 @@ describe('Task Suite', () => {
       expect(tasks.at(0).fn).toHaveBeenCalled();
       expect(tasks.at(1).fn).toHaveBeenCalled();
     }, 15e3);
+
+    it('should only run tasks that are due - with fake timers (fast)', () => {
+      jest.useFakeTimers();
+
+      const tasks = [
+        {
+          name: 'Task-Will-Run-In-5-Seconds',
+          dueAt: new Date(Date.now() + 5000),
+          fn: jest.fn(),
+        },
+        {
+          name: 'Task-Will-Run-In-10-Seconds',
+          dueAt: new Date(Date.now() + 10000),
+          fn: jest.fn(),
+        },
+        {
+          name: 'Task-Will-Run-In-15-Seconds',
+          dueAt: new Date(Date.now() + 15000),
+          fn: jest.fn(),
+        },
+      ];
+
+      tasks.forEach((task) => _task.save(task));
+
+      _task.run(200);
+
+      // time = 4s
+      jest.advanceTimersByTime(4000);
+
+      expect(tasks.at(0).fn).not.toHaveBeenCalled();
+      expect(tasks.at(1).fn).not.toHaveBeenCalled();
+      expect(tasks.at(2).fn).not.toHaveBeenCalled();
+
+      // time = 4s + 5s = 9s
+      jest.advanceTimersByTime(5000);
+
+      expect(tasks.at(0).fn).toHaveBeenCalled();
+      expect(tasks.at(1).fn).not.toHaveBeenCalled();
+      expect(tasks.at(2).fn).not.toHaveBeenCalled();
+
+      // time = 9s + 5s = 14s
+      jest.advanceTimersByTime(5000);
+
+      expect(tasks.at(0).fn).toHaveBeenCalled();
+      expect(tasks.at(1).fn).toHaveBeenCalled();
+      expect(tasks.at(2).fn).not.toHaveBeenCalled();
+
+      // time = 14s + 5s = 19s
+      jest.advanceTimersByTime(5000);
+
+      expect(tasks.at(0).fn).toHaveBeenCalled();
+      expect(tasks.at(1).fn).toHaveBeenCalled();
+      expect(tasks.at(2).fn).toHaveBeenCalled();
+
+      jest.useRealTimers();
+    });
   });
 });
