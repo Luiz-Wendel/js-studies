2a915b6824c7e07e8852ffd49718e159822a59f4
Luiz Wendel <luiz.balbino@mercadolivre.com>
2024-03-01 15:20:27 -0300

    test: add Task run function - clear interval test

--- tajs/02-concepts/05-code-coverage/test/task.test.js
+++ tajs/02-concepts/05-code-coverage/test/task.test.js
@@ -1,7 +1,11 @@
 import { describe , beforeEach, it, jest, expect } from '@jest/globals';
-import { setTimeout } from 'timers/promises';
 
 import Task from '../src/task';
+
+const FUNC_NAMES = {
+  LOG: console.log.name,
+  CLEAR_INTERVAL: global.clearInterval.name,
+};
 
 describe('Task Suite', () => {
   let _logMock;
@@ -10,7 +14,7 @@ describe('Task Suite', () => {
   beforeEach(() => {
     _logMock = jest.spyOn(
       console,
-      console.log.name,
+      FUNC_NAMES.LOG,
     ).mockImplementation();
 
     _task = new Task();
@@ -71,6 +75,47 @@ describe('Task Suite', () => {
       expect(tasks.at(2).fn).toHaveBeenCalled();
 
       jest.useRealTimers();
+    });
+
+    it('should clear the interval when there are no more tasks', () => {
+      jest.useFakeTimers();
+
+      jest.spyOn(
+        global,
+        FUNC_NAMES.CLEAR_INTERVAL,
+      ).mockImplementation();
+
+      const tasks = [
+        {
+          name: 'Task-Will-Run-In-5-Seconds',
+          dueAt: new Date(Date.now() + 5000),
+          fn: jest.fn(),
+        },
+        {
+          name: 'Task-Will-Run-In-10-Seconds',
+          dueAt: new Date(Date.now() + 10000),
+          fn: jest.fn(),
+        },
+        {
+          name: 'Task-Will-Run-In-15-Seconds',
+          dueAt: new Date(Date.now() + 15000),
+          fn: jest.fn(),
+        },
+      ];
+
+      tasks.forEach((task) => _task.save(task));
+
+      _task.run(200);
+
+      // time = 10s
+      jest.advanceTimersByTime(1e4);
+
+      expect(global.clearInterval).not.toHaveBeenCalled();
+
+      // time = 10s + 5.2s = 15.2s
+      jest.advanceTimersByTime(52e2);
+
+      expect(global.clearInterval).toHaveBeenCalled();
     });
   });
 });
